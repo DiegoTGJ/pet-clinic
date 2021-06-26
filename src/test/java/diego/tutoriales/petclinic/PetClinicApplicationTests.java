8d95aeba11e427c9b7e813274a6a1ddf6ffe93af
PDTG <diegotgjobs@gmail.com>
2021-06-26 05:35:06 -0400

    Initial commit

---
+++ src/test/java/diego/tutoriales/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package diego.tutoriales.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
