37e9a376476d8d4ca09831ab30505d034d7fb351
PDTG <diegotgjobs@gmail.com>
2021-06-27 11:22:51 -0400

    TUT-10 #close Implemented map based services

--- src/main/java/diego/tutoriales/petclinic/services/PetService.java
+++ src/main/java/diego/tutoriales/petclinic/services/PetService.java
@@ -3,7 +3,5 @@ package diego.tutoriales.petclinic.services;
 import diego.tutoriales.petclinic.model.Pet;
 
 public interface PetService extends  CrudService<Pet, Long>{
-	
-
 
 }
---
+++ src/main/java/diego/tutoriales/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package diego.tutoriales.petclinic.services.map;
+
+import diego.tutoriales.petclinic.model.Vet;
+import diego.tutoriales.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
