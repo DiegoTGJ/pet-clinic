c8766050d4695e4883a8fbd0b6cdad015f847437
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2021-07-25 17:46:54 -0400

    added specialties, added to vet. Closes #11

--- src/main/java/diego/tutoriales/petclinic/model/Vet.java
+++ src/main/java/diego/tutoriales/petclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package diego.tutoriales.petclinic.model;
 
-public class Vet extends Person {
+import java.util.Set;
 
+public class Vet extends Person {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
